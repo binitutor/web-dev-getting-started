248d174440745a71c0a199ec6b71863ab2e19a6e
BiniTutor <awakisew@gmail.com>
2025-04-15 23:16:35 -0400

    converted html to php

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php 
+    // PHP CODE GOES HERE ....
+
+    // capturing requests
+    // $_GET[]
+    // $_POST[]
+    // $_COOKIE[]
+    // $_SESSION[]
+    // $_SERVER[]
+
+    // functions - utility
+    function fnName( $val ){
+        return null;
+    }
+
+    // classes - authentication and db helper
+    class ClassName {
+        ...
+    }
+
+    // declare variable
+    $myName = 'Bini';
+
+    // concatenaete string using .
+    echo '
+    <h1 class="title-text" id="title-1">
+        My name is '. $myName .'
+    </h1>
+    ';
+
+?>
